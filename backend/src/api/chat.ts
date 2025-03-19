7d8bf5a58ae516e8611a1f2afd18bf1972f20301
Lucas Gelfond <lucas_gelfond@brown.edu>
2025-03-19 03:23:53 -0700

    one more try at private networking

--- backend/src/api/chat.ts
+++ backend/src/api/chat.ts
@@ -5,12 +5,11 @@ dotenv.config();
 
 const IS_PRODUCTION = !process.env.IS_DEV;
 const BASE_URL = IS_PRODUCTION
-  ? "anythingllm.railway.internal"
+  ? "http://anythingllm.railway.internal"
   : "https://anythingllm-production-047a.up.railway.app";
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  console.log();
   try {
     const response = await fetch(`${BASE_URL}/api/v1/workspace/jfk/chat`, {
       method: "POST",
